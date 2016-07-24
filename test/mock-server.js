8252a294de96a7fd3577dd6d37483f3b9044f7db
Naugtur <naugtur@gmail.com>
2016-07-24 18:14:24 +0200

    finalize fix for xhr.response value checking, add tests for edge-cases (need fixes for IEs)

--- test/mock-server.js
+++ test/mock-server.js
@@ -3,13 +3,29 @@ module.exports = function (req, res) {
     if (req.url === '/mock/200ok') {
         res.statusCode = 200
         res.end('')
+    } else if (req.url === '/mock/200json-empty') {
+        res.statusCode = 200
+        res.setHeader('content-type','application/json')
+        res.end()
+    } else if (req.url === '/mock/200json-false') {
+        res.statusCode = 200
+        res.setHeader('content-type','application/json')
+        res.end('false')
+    } else if (req.url === '/mock/200xml-empty') {
+        res.statusCode = 200
+        res.setHeader('content-type','application/xml')
+        res.end('<?xml version="1.0" ?>')
+    } else if (req.url === '/mock/200xml') {
+        res.statusCode = 200
+        res.setHeader('content-type','application/xml')
+        res.end('<?xml version="1.0" ?><x>x</x>')
     } else if (req.url === '/mock/no-content') {
         res.statusCode = 204
-        res.end('')
+        res.end()
     } else if (req.url === '/mock/timeout') {
         setTimeout(function() {
             res.statusCode = 200
             res.end()
         }, 100)
-    } 
+    }
 }
