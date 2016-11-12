987b6d3ffcaabab0e5f7fcb78e3b64c09a36a212
Naugtur <naugtur@gmail.com>
2016-11-12 12:40:39 +0100

    simplify echo mock implementation

--- test/mock-server.js
+++ test/mock-server.js
@@ -1,5 +1,5 @@
-module.exports = function (req, res) {
-    console.log('mock:',req.url)
+module.exports = function(req, res) {
+    console.log('mock:', req.url)
     if (req.url === '/mock/200ok') {
         res.statusCode = 200
         res.end('')
@@ -7,18 +7,12 @@ module.exports = function (req, res) {
         res.statusCode = 204
         res.end('')
     } else if (req.url === '/mock/echo') {
-        var body = []
-        req.on('data', function(chunk) {
-            body.push(chunk)
-        })
-        req.on('end', function() {
-            res.statusCode = 200
-            res.end(Buffer.concat(body).toString())
-        })
+        res.statusCode = 200
+        req.pipe(res)
     } else if (req.url === '/mock/timeout') {
         setTimeout(function() {
             res.statusCode = 200
             res.end()
         }, 100)
-    } 
+    }
 }
