d1a9ff9eb9f1c7beff2bb5e1397b8ed4089e0f75
Naugtur <naugtur@gmail.com>
2017-04-28 14:40:19 +0200

    remove new keywords and use .bind

--- index.js
+++ index.js
@@ -147,16 +147,14 @@ function _createXHR(options) {
     xhr.onreadystatechange = readystatechange
     xhr.onload = loadFunc
 
-    xhr.onerror = function () {
-        errorFunc(new Error('Unknown XMLHttpRequest Error'))
-    }
+    xhr.onerror = errorFunc.bind({}, Error('Unknown XMLHttpRequest Error'))
 
     xhr.onabort = function(){
         aborted = true;
     }
-    xhr.ontimeout = function () {
-        errorFunc(new Error('XMLHttpRequest Timeout'))
-    }
+    
+    xhr.ontimeout = errorFunc.bind({}, Error('XMLHttpRequest Timeout'))
+
     xhr.open(method, uri, !sync, options.username, options.password)
     //has to be after open
     if(!sync) {
